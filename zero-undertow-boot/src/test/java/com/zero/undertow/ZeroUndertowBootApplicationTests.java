31ae1cb23cff2ec310c32b101e9fb0a3af7b91c8
Micro-Organism <coolrabbit1993@gmail.com>
2024-10-28 11:09:36 +0800

    feat: Spring Boot 集成 Undertow

---
+++ zero-undertow-boot/src/test/java/com/zero/undertow/ZeroUndertowBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.undertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroUndertowBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
